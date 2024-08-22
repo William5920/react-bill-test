6dd7e509de7004c36c91144df0bb63166d6973df
unknown <1101044179@qq.com>
2024-08-22 20:54:18 +0800

    完成主题颜色配置

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import { RouterProvider } from 'react-router-dom'
 import router from '@/router'
 
+// 引入主题颜色配置
+import './theme.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RouterProvider router={router} />
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
+import { Button } from 'antd-mobile'
 
 const Layout = () => {
   return (
     <div>
       <Outlet />
       Layout
+      <Button color="primary">主题色配置</Button>
     </div>
   )
 }
