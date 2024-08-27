515016c596ed5c451d154f5fcf4caccc83083380
unknown <1101044179@qq.com>
2024-08-27 20:03:59 +0800

    使用redux管理账单数据

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom'
 import router from '@/router'
+import {Provider} from 'react-redux'
+import store from './store'
 
 // 引入主题颜色配置
 import './theme.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={router} />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
 
