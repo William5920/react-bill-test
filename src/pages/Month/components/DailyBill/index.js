ba6dc70884f297583eb17cd9689b7380cbe141d6
unknown <1101044179@qq.com>
2024-09-10 17:32:56 +0800

    月度账单-点击展开收起日账单列表

--- src/pages/Month/components/DailyBill/index.js
+++ src/pages/Month/components/DailyBill/index.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 import './index.scss'
-import {useMemo} from 'react'
+import {useMemo, useState} from 'react'
 import {billTypeToName} from '@/contants/index'
 
 const DailyBill = ({date, billList}) => {
@@ -19,13 +19,15 @@ const DailyBill = ({date, billList}) => {
       total: pay + income
     }
   }, [billList])
+  // 账单列表显隐
+  const [visible, setVisible] = useState(false)
 
   return (
     <div className={classNames('dailyBill')}>
       <div className="header">
         <div className="dateIcon">
           <span className="date">{date}</span>
-          <span className={classNames('arrow')}></span>
+          <span className={classNames('arrow', visible && 'expand')} onClick={() => setVisible(!visible)}></span>
         </div>
         <div className="oneLineOverview">
           <div className="pay">
@@ -43,7 +45,7 @@ const DailyBill = ({date, billList}) => {
         </div>
       </div>
       {/* 单日列表 */}
-      <div className="billList">
+      <div className="billList" style={{display: visible ? 'block' : 'none'}}>
         {billList.map(item => {
           return (
             <div className="bill" key={item.id}>
